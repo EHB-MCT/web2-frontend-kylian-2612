44938dc7ee4245001a760cda0ee2fbac6b2b89b4
kylian-2612 <71694721+kylian-2612@users.noreply.github.com>
2022-08-10 20:55:38 +0200

    created working frontend without DB or API

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 const path = require('path'); 
-
 module.experts = {
     entry: './docs/main.js',
     output: {
-        filename: 'main.js', 
         path: path.resolve(__dirname, 'docs'), 
+        filename: 'main.js',
     }, 
+    node: 'production',
+    watch: true
 };
